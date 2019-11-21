c9b1e56aaca1c2cfd5e1ae48c94b9da493772475
cuncheniver <56687227+cuncheniver@users.noreply.github.com>
2019-11-21 14:09:52 +0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,46 @@
+
+<!-- Form-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" href="../DA_cuoiki/image/logo.PNG">
+    <link rel="stylesheet" href="../DA_cuoiki/css/st1.css">
+    <title>Login</title>
+</head>
+<body>
+        <div class="form">
+                <div class="form-toggle"></div>
+                <div class="form-panel one">
+                    <div class="form-header">
+                        <h1>Account Login</h1>
+                    </div>
+                    <div class="form-content">
+                        <form>
+                            <div class="form-group"><label for="username">Username</label><input type="text" id="username" name="username" required="required" /></div>
+                            <div class="form-group"><label for="password">Password</label><input type="password" id="password" name="password" required="required" /></div>
+                            <div class="form-group"><label class="form-remember"><input type="checkbox"/>Remember Me</label><a class="form-recovery" href="#">Forgot Password?</a></div>
+                            <div class="form-group"><button type="submit">Log In</button></div>
+                        </form>
+                    </div>
+                </div>
+                <div class="form-panel two">
+                    <div class="form-header">
+                        <h1>Register Account</h1>
+                    </div>
+                    <div class="form-content">
+                        <form>
+                            <div class="form-group"><label for="username">Username</label><input type="text" id="username" name="username" required="required" /></div>
+                            <div class="form-group"><label for="password">Password</label><input type="password" id="password" name="password" required="required" /></div>
+                            <div class="form-group"><label for="cpassword">Confirm Password</label><input type="password" id="cpassword" name="cpassword" required="required" /></div>
+                            <div class="form-group"><label for="email">Email Address</label><input type="email" id="email" name="email" required="required" /></div>
+                            <div class="form-group"><button type="submit">Register</button></div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <script src="../DA_cuoiki/js/st1.js"></script>
+</body>
+</html>
